e4b3a338091b9758df0044a8b7fce4ef7eba18b3
genuino12 <lucasgenuino567@gmail.com>
2024-09-21 15:51:36 -0300

    atualizar

--- Controle/eventoCtrl.js
+++ Controle/eventoCtrl.js
@@ -1,8 +1,18 @@
+import evento from "../model/Eventos.js";
 export default class eventoctrl{
     gravar(requisicao,resposta){ 
         if(requisicao.method == "Post" && requisicao.is("application/json")){
             const dados = requisicao.body;
-            
+            const nome_evento = dados.nome_evento;
+            const descricao = dados.descricao;
+            const data_hora = dados.data_hora;
+            const local = dados.local;
+            const preco = dados.preco;
+            const ingressos = dados.ingressos;
+
+            if(nome_evento && descricao && data_hora && local && preco && ingressos){
+                const evento = new evento()
+            }
         }else{
             resposta.status(405).json({
                 "Status": false,
