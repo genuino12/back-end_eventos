f5a8a6a06e63e5c15efc39ec2a03cf371d970a6f
genuino12 <lucasgenuino567@gmail.com>
2024-09-21 23:09:33 -0300

    atualizar

--- DAO/conexao.js
+++ DAO/conexao.js
@@ -1,22 +1,18 @@
 import mysql from 'mysql2/promise';
-
 export default async function conectar() {
     if (global.poolConexões) {
         return await global.poolConexões.getConnection();
-    } else {
-    
-        global.poolConexões = mysql.createPool({
+    }
+    else {
+        const connection = await mysql.createConnection({
             host: '127.0.0.1',
             port: 3306,
             user: 'root',
             password: '',
             database: 'eventos_db',
-            insecureAuth: true,
-            waitForConnections: true,
-            connectionLimit: 10,
-            queueLimit: 0
+            insecureAuth: true
         });
-        
-        return await global.poolConexões.getConnection();
+        connection.connect();
+        return connection;
     }
 }
