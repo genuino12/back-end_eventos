e462f3b3cf962e51945d581a9fa17a6c161adde9
genuino12 <lucasgenuino567@gmail.com>
2024-09-21 21:40:16 -0300

    atualização

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const porta = 4000;
 
 
 app.use(express.json());
-app.use('/evetno', ???????);
+app.use('/evetno',);
 
 app.listen(porta,host, () => {
 console.log(`Servidor Iniciado em https://${host}:${porta}`);
