bbb3b7551d50a49e8c386a371d24ba71e18cca72
genuino12 <lucasgenuino567@gmail.com>
2024-09-21 21:04:54 -0300

    atualização

--- Controle/eventoCtrl.js
+++ Controle/eventoCtrl.js
@@ -1,7 +1,8 @@
-import evento from "../model/Eventos.js";
+import eventos from "../model/Eventos.js";
+
 export default class eventoctrl{
     gravar(requisicao,resposta){ 
-        if(requisicao.method == "Post" && requisicao.is("application/json")){
+        if(requisicao.method == "POST" && requisicao.is("application/json")){
             const dados = requisicao.body;
             const nome_evento = dados.nome_evento;
             const descricao = dados.descricao;
@@ -11,19 +12,73 @@ export default class eventoctrl{
             const ingressos = dados.ingressos;
 
             if(nome_evento && descricao && data_hora && local && preco && ingressos){
-                const evento = new evento()
+                const evento = new eventos(nome_evento,descricao,data_hora,local,preco,ingressos);
+
+                evento.incluir().then(() => {
+                    resposta.status(201).json({
+                 "Status": true,
+                "mensagem": "Evento Incluído com Sucesso"
+                })
+                }).catch((erro) => {
+                    resposta.status(500).json({
+                "Status": false,
+                "mensagem": "Erro ao Incluir Evento" + erro.message
+                    })
+                });
             }
         }else{
             resposta.status(405).json({
                 "Status": false,
-                "mensagem": "Requisição Invalida",
+                "mensagem": "Requisição Invalida, informe todos dados do evento"
             })
         }
     };
+}
 
-    alterar(requisicao,resposta){};
+    alterar(requisicao,resposta){
+        if(requisicao.method == "PUT" || requisicao.method == "PATCH" && requisicao.is("application/json")){
+            const dados = requisicao.body;
+            const nome_evento = dados.nome_evento;
+            const descricao = dados.descricao;
+            const data_hora = dados.data_hora;
+            const local = dados.local;
+            const preco = dados.preco;
+            const ingressos = dados.ingressos;
 
-    excluir(requisicao,resposta){};
+            if(nome_evento && descricao && data_hora && local && preco && ingressos){
+                const evento = new eventos(nome_evento,descricao,data_hora,local,preco,ingressos);
+
+                evento.alterar().then(() => {
+                    resposta.status(201).json({
+                 "Status": true,
+                "mensagem": "Evento alterado com Sucesso"
+                    });
+            }).catch((erro) => {
+                resposta.status(500).json({
+            "Status": false,
+            "mensagem": "Erro ao Incluir Evento" + erro.message
+                })
+            });
+        }
+        else{
+            resposta.status(400).json({
+                "Status": false,
+                "mensagem": "Requisição Invalida, informe todos dados do evento"
+        });
+        }
+    }else{
+        resposta.status(405).json({
+            "Status": false,
+            "mensagem": "Requisição Invalida, informe todos dados do evento"
+        });
+    }
+}
+    excluir(requisicao,resposta){
+        if(requisicao.method == "Delete" && requisicao.is("application/json")){
+            const dados = requisicao.body;
+            const codigo = dados.nome_evento;
+    };
+}
 
     consulta(requisicao,resposta){};
-}
+   
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app =  express();
 const host = "127.0.0.1";
 const porta = 4000; 
 
+
+app.use(express.json());
+
 app.listen(porta,host, () => {
 console.log(`Servidor Iniciado em http://${host}:${porta}`);
 });
