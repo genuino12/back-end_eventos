82c63f0875ead5d5889f0faf95aab315464400ea
genuino12 <lucasgenuino567@gmail.com>
2024-10-01 22:07:51 -0300

    atualização

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import rota_evento from "./Rotas/rota_evento.js";
+import cors from "cors";
 
 const app =  express();
 const host = "127.0.0.1";
 const porta = 4000; 
 
-
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 app.use('/evento', rota_evento);
 
